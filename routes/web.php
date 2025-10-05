87151203aed7496d6f668622fa5400efb1fac67a
kevquino <109962506+kevquino@users.noreply.github.com>
2025-10-05 10:29:25 +0800

    creating news

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,10 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Update this line to point to the settings folder
+Route::get('news', function () {
+    return Inertia::render('News');
+})->middleware(['auth', 'verified'])->name('news');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
