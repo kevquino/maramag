867d44e818a3dce81453c1fdcc05a5836899be3c
kevquino <109962506+kevquino@users.noreply.github.com>
2025-10-13 19:09:17 +0800

    app logo icon corrected when collapse

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+
+interface Props {
+    isCollapsed?: boolean;
+}
+
+defineProps<Props>();
 </script>
 
 <template>
-    <div
-        class="flex  size-8 items-center justify-center rounded-full  "
-    >
-        <AppLogoIcon class="size-auto fill-current text-white dark:text-black" />
-    </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >LGU Maramag</span
+    <div class="flex items-center w-full" :class="isCollapsed ? 'justify-center' : 'justify-start'">
+        <div class="flex size-8 items-center justify-center rounded-full shrink-0">
+            <AppLogoIcon class="size-auto fill-current text-white dark:text-black" />
+        </div>
+        
+        <div 
+            v-if="!isCollapsed" 
+            class="ml-2 grid flex-1 text-left text-sm"
         >
+            <span class="truncate leading-tight font-semibold">LGU Maramag</span>
+        </div>
     </div>
 </template>
